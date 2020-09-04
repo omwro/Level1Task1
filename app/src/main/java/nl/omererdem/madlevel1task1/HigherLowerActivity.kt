933f65769d6ad54ba1a61146567e33d687ba9ac3
Omer Erdem <m.o.erdem@outlook.com>
2020-09-04 23:03:19 +0200

    #5 Init variables

--- app/src/main/java/nl/omererdem/madlevel1task1/HigherLowerActivity.kt
+++ app/src/main/java/nl/omererdem/madlevel1task1/HigherLowerActivity.kt
@@ -2,8 +2,13 @@ package nl.omererdem.madlevel1task1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import nl.omererdem.madlevel1task1.databinding.ActivityHigherLowerBinding
 
 class HigherLowerActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityHigherLowerBinding
+    private var currentThrow: Int = 1
+    private var lastThrow: Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_higher_lower)
